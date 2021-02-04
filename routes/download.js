8c0c8694b60edf85e51854bb5ea203eba654f584
Ubuntu <manh@getlink-vps.ne41xcx3fvke1greylbniweojg.hx.internal.cloudapp.net>
2021-02-04 17:16:11 +0000

    v1.2 retrieve limiter at get-link & fix shorten url

--- routes/download.js
+++ routes/download.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-const rateLimit = require('express-rate-limit');
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 2
-});
 
-router.get('/', limiter, (req, res) => {
-    const link = req.query.link.replace(/ /g, "+");
-    console.log(link);
+router.get('/', async (req, res) => {
+    const link = new Buffer.from(req.query.link, 'base64').toString('ascii');
     fetch(link).then(file => {
         file.body.pipe(res);
         file.body.on('end', () => res.status(200).end());
--- routes/get-link.js
+++ routes/get-link.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const redis = require('redis');
 const client = redis.createClient();
 const fetch = require('node-fetch');
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 2
+});
 
 const get = require('../services/redis-get');//get value from redis
 const login = require('../services/login');
@@ -18,23 +23,26 @@ checkLink = async function (req, res, next) {
     else res.status(404).send("File not found.");
 }
 
-router.get('/:linkcode', checkLink, async (req, res) => {
+router.get('/:linkcode', checkLink, limiter, async (req, res) => {
     let token = await get.key(client, "token");
     let cookie = await get.key(client, "cookie");
     const linkcode = req.params.linkcode;
 
-    await logger.info(req.ip + ' GET /download/' + linkcode);
-    getDownloadLink(token, cookie, linkcode)
-        .then(async (link) => {
-            link = req.protocol + '://' + req.get('host') + '/download?link=' + link;
+    await logger.info(req.ip + ' GET /get/' + linkcode);
+
+    let running = true;
+    while (running) {
+        let link = await getDownloadLink(token, cookie, linkcode).catch(err => logger.error('get link failed: ' + err));
+        if (link) {
+            const base64link = new Buffer.from(link).toString('base64');
+            link = req.protocol + '://' + req.get('host') + '/redirect.html?link=' + encodeURIComponent(base64link);
             const shortenRes = await fetch('https://megaurl.in/api?api=6137549b1c06c25a2153b4a10f050643aef11f34&url=' + link);
             const shorten = await shortenRes.json();
             if (shorten.status == 'error') logger.error('shorten url error: ' + shorten.message);
             else logger.info('shortenUrl: ' + shorten.shortenedUrl);
             res.status(200).send(shorten.shortenedUrl);
-        })
-        .catch(async (err) => {
-            await logger.error('get link failed: ' + err);
+            running = false;
+        } else {
             let current = await get.key(client, "current");
             let accounts = await get.list(client, "accounts");
             current = Number(current);
@@ -52,9 +60,10 @@ router.get('/:linkcode', checkLink, async (req, res) => {
                 [token, cookie] = await login(email, password);
                 const data = ["token", token, "cookie", cookie, "current", current];
                 await update(client, data);
-                res.redirect('/get/' + linkcode);
             }
-        });
+        }
+
+    }
 });
 
 module.exports = router;
